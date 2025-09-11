889e7a270f87c5c298d445961f7477f2c1cb69d3
kgalvanserrano <kevin.galvan@live.com>
2025-09-10 19:10:29 -0700

    refactor: improve rendering of transactions in TransactionList component

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -3,19 +3,20 @@
 // style it so income/expenses are visually distinct
 // red for expenses, green for income
 
-
 import React from "react";
 import { useState } from "react";
 
 export default function TransactionList({ transactions }) {
-    transactions.sort((a, b) => b.id - a.id); // sort by most recent
-    return (
-                <ul className="transaction-list">
-                    {transactions.map((tx) => (
-                        <li key={tx.id} className={tx.type === "income" ? "green" : "red"}>
-                            {tx.description}: ${tx.amount} ({tx.type})
-                        </li>
-                    ))}
-                </ul>
-            );
-        }
+  transactions.sort((a, b) => b.id - a.id); // sort by most recent
+  return (
+    <ul className="transaction-list">
+      {[...transactions] // make a shallow copy
+        .sort((a, b) => b.id - a.id) // sort the copy
+        .map((tx) => (
+          <li key={tx.id} className={tx.type === "income" ? "green" : "red"}>
+            {tx.description}: ${tx.amount.toFixed(2)} ({tx.type})
+          </li>
+        ))}
+    </ul>
+  );
+}
